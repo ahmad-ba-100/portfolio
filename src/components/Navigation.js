320a11af7007e95031e8a0bd42e1028dee1030fb
Amadou BA <bamadoulife@gmail.com>
2022-10-12 17:55:34 +0100

    c

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,7 +6,7 @@ const Navigation = () => {
         <div className='sidebar'>
             <div className='id'>
             <div className='idContent'>
-                <img src='./media/Amadou-BA.jpg' alt='profil-pic'/>
+                <img src='./media/Amadou-BA.jpeg' alt='profil-pic'/>
                 <h3>Amadou BA</h3>
                 
                 
